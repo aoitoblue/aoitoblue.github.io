31eaae939281b0b01a049ef0f964fdf6b3273919
aoitoblue <37381335+aoitoblue@users.noreply.github.com>
2018-03-19 03:41:05 +0900

    Update pixiAframeAR.js

--- CubismARjsSample/js/pixiAframeAR.js
+++ CubismARjsSample/js/pixiAframeAR.js
@@ -161,7 +161,7 @@ window.onload = function () {
 		plane.setAttribute('width', '5');
 		//マーカーを基準にしたモデルの相対位置
 		plane.setAttribute('position', '0 1 -1');
-		var stand = stand_mode ? '-45 0 0' : '-90 0 0';
+		var stand = stand_mode ? '-15 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);
 
